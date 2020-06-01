b8ac00194bc3225b3f35e3c534be431a24710601
betheunique <abhisheksavitarai@gmail.com>
2020-06-01 07:14:46 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.148348cc81da35c4cdffd7aa059cd614.js"
+  "/precache-manifest.fff1e5f1e0c9492e75cc4206494c2baa.js"
 );
 
 self.addEventListener('message', (event) => {
