a7c7c1716cd3e864c8497b6abb6b76113045eb92
betheunique <abhisheksavitarai@gmail.com>
2020-06-01 08:28:54 +0200

    Updates

---
+++ precache-manifest.7ad2ed15aa1a7cbad02c7fbbb84d3605.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d7a5e40f8cb6f5daa2ec13fd0ec7a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a6f05bd1a1be30ea181",
+    "url": "/static/css/main.8b72132f.chunk.css"
+  },
+  {
+    "revision": "8d8324857e8c33145593",
+    "url": "/static/js/2.5b304c54.chunk.js"
+  },
+  {
+    "revision": "fbb8ef2bf304fca191b26954752f01a4",
+    "url": "/static/js/2.5b304c54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a6f05bd1a1be30ea181",
+    "url": "/static/js/main.5bc1873d.chunk.js"
+  },
+  {
+    "revision": "bb90005e0f6cf3819290",
+    "url": "/static/js/runtime-main.613e5177.js"
+  },
+  {
+    "revision": "1b512082d661354c6d687eb8920a7afc",
+    "url": "/static/media/background_video.1b512082.mp4"
+  },
+  {
+    "revision": "65f8709df0f0bfa7d0a05d7f9517ba07",
+    "url": "/static/media/loader.65f8709d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fff1e5f1e0c9492e75cc4206494c2baa.js"
+  "/precache-manifest.7ad2ed15aa1a7cbad02c7fbbb84d3605.js"
 );
 
 self.addEventListener('message', (event) => {
